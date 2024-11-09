996c2d53567799acd678d3eefda10b4bbe812edb
LohYenKuan <lohyenkuan@gmail.com>
2024-11-09 12:50:48 +0800

    add userType in user
    in the previous commit I didnt initialize the other class, only purchase
    manager, sales manager, and user. I also added the interface managable
    and abstract user type

--- src/assignment/User.java
+++ src/assignment/User.java
@@ -5,16 +5,18 @@ public class User {
   private String username;
   private String email;
   private String password;
+  private UserType userType;
 
   // Constructor
   public User() {
   }
 
-  public User(String userId, String username, String email, String password) {
+  public User(String userId, String username, String email, String password, UserType userType) {
     this.userId = userId;
     this.username = username;
     this.email = email;
     this.password = password;
+    this.userType = userType;
   }
 
   // Getters and setters
@@ -50,9 +52,18 @@ public class User {
     this.password = password;
   }
 
+  public UserType getUserType() {
+    return userType;
+  }
+
+  public void setUserType(UserType userType) {
+    this.userType = userType;
+  }
+
   // methods
   public void login() {
-    // TODO: implement login
+    // TODO: implement logic for login
+    userType.validateAcess();
   }
 
   public void logout() {
