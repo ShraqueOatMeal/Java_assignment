009ceda9d669d534a5d01ec0945b38cead34bf11
LohYenKuan <lohyenkuan@gmail.com>
2024-11-11 17:07:03 +0800

    fix conflicts

--- src/assignment/SalesManager/salesManager.java
+++ src/assignment/SalesManager/salesManager.java
@@ -1,31 +1,6 @@
 package assignment.SalesManager;
 
 import assignment.UserType;
-
-public class SalesManager extends UserType {
-  public void createRequisition() {
-    // Implementation
-  }
-
-  public void viewItems() {
-    // Implementation
-  }
-
-  public void enterDailySales() {
-    // Implementation
-  }
-
-  public void generateSalesReport() {
-    // Implementation
-  }
-
-  public void checkStockLevel() {
-    // Implementation
-  }
-
-  public void viewPurchaseOrders() {
-    // Implementation
-  }
 import assignment.Login;
 
 /**
