0b98ee78125ff5f781af955811f56fb28e7c8f69
LohYenKuan <lohyenkuan@gmail.com>
2024-11-16 12:40:44 +0800

    add few more colums to view requisition from purchase manager as required from the assignment

--- src/assignment/PurchaseManager/viewRequisition.java
+++ src/assignment/PurchaseManager/viewRequisition.java
@@ -163,13 +163,14 @@ public class viewRequisition extends javax.swing.JFrame {
 
         },
         new String[] {
-            "Item ID", "Item Name", "Item Quantity", "Item Status"
+            "Item ID", "Item Name", "Item Quantity", "Item Status", "Sales Manager", "Supplier ID", "Date"
         }) {
       Class[] types = new Class[] {
-          java.lang.Object.class, java.lang.Object.class, java.lang.Integer.class, java.lang.String.class
+          java.lang.Object.class, java.lang.Object.class, java.lang.Integer.class, java.lang.String.class,
+          java.lang.String.class, java.lang.String.class, java.lang.String.class
       };
       boolean[] canEdit = new boolean[] {
-          false, false, false, false
+          false, false, false, false, false, false, false
       };
 
       public Class getColumnClass(int columnIndex) {
